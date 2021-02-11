1f2336671f8f65fc12783f1c3a2a7b424ec88681
sarrazol <sebastianarrazola@gmail.com>
2021-02-10 19:29:16 -0500

    init portfolio routes, need to set up the components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router,Route, Switch } from "react-router-dom";
+// import UserAuth from "./components/UserAuth/index";
+// import PrivateRoute from "./components/PrivateRoute/index";
+// import PreventReverse from "./components/PreventReverse/index";
+import history from "./utils/history";
+import Portfolio from "./pages/Rules/index"
+import About from "./pages/Login/index";
+import Contact from "./pages/SignUp/index";
+// import Game from "./pages/Game/index";
+// import SoundSuite from "./components/SoundSuite/index"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container h-100 d-flex justify-content-center">
+      <div className="jumbotron my-auto">
+
+        <Router history={history}>
+          <Switch>
+            
+            <Route path="/" exact>
+              <Portfolio />
+            </Route>
+
+            <Route path="/about">
+              <About />
+            </Route>
+
+            <Route path="/contact">
+              <Contact />
+            </Route>
+
+          </Switch>
+        </Router>
+      
+      </div>
     </div>
   );
 }
