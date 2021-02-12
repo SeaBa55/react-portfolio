1777debea18581d0c5245d77b4444f60caa58624
sarrazol <sebastianarrazola@gmail.com>
2021-02-11 19:09:43 -0500

    set up rough page routing for the portfolio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
 import { Router,Route, Switch } from "react-router-dom";
-// import UserAuth from "./components/UserAuth/index";
-// import PrivateRoute from "./components/PrivateRoute/index";
-// import PreventReverse from "./components/PreventReverse/index";
 import history from "./utils/history";
-import Portfolio from "./pages/Rules/index"
-import About from "./pages/Login/index";
-import Contact from "./pages/SignUp/index";
-// import Game from "./pages/Game/index";
-// import SoundSuite from "./components/SoundSuite/index"
+import Portfolio from "./pages/portfolio/index"
+import About from "./pages/about/index";
+import Contact from "./pages/contact/index";
 
 function App() {
   return (
---
+++ src/pages/about/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+
+    return (
+        <>
+           <div class="about-me-text">
+                <p>Originally from Bogota, Colombia, my family and I emigrated to the states in search of peace away from the narco-state that had become our home at the time. The year was 1997, and we were off to the land of opportunity where we found a pleasant place to call home in Montgomery County, Maryland.</p>
+                <p>Over the next 20 years I spent living in Montgomery County, I grew passionate for music production, engineering and mechanical design, as these fields offered a vessel for my creativity to flourish. Due to a growing interest in design and engineering I decided to study mechanical engineering at the University of Maryland. It was in this institution where I was exposed to mechatronics which is where mechanical, electrical, and software/computer engineering overlap.</p>
+                <p>During my time in college, and after graduation I worked on various mechatronics, and product design projects such as active aero, design and CNC manufacturing of an ultra slim billet aluminum oil pan for the formula SAE race team I took part in. For my final mechatronics’ class project I designed and built a desk top Peltier HVAC system using a standalone atmega328p microcontroller that I programmed to interface with a user through a control knob and display for dialing-in a desired temperature set point. This project further spun off as an English class project in which I created educational material that aimed to walk students through the fundamentals of electronics  before having to follow a step-by-step manual on how to construct the Peltier HVAC system. The educational material/ build manual was distributed through a collaboration with the mpambara-cox foundation to high school students in Uganda that expressed interested in stem!</p>
+                <p>While there were so many other projects, I inevitably fell in love with the software side of mechatronics. I really like how quickly, effortlessly software enables the deployment of great project ideas. While working in my second year at Ford Motor Company, I enrolled in an introductory course on microprocessors at the University of Michigan. Through this class I was exposed to the very hardware that makes software possible, and the programing language that makes it all possible behind the scenes, assembly. Working with assembly language opened my mind to a new-found understanding for how software manages hard-coded commands stored in memory, to make the magic of computation happen. From there it all clicked; I spent a decade “close to the metal” and now I am fully prepared to take on web and software development. Join me, as I attempt to speed-run to the top ranks of tech!</p>
+                <p>Welcome to my first responsive site! I hope to connect, collaborate with you, and hear about your story.</p>
+            </div> 
+        </>
+    );
+}
+  
+export default About;
