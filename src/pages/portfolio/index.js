ea5cc9700093aff3c25ec16c886794b7aceed83b
sarrazol <sebastianarrazola@gmail.com>
2021-03-24 10:11:40 -0400

    added portfolio page

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Col, Container } from "react-bootstrap";
+import { CardDeck, Card, Col, Container, Row } from "react-bootstrap";
 import './style.css';
 
 function Portfolio() {
@@ -7,35 +7,130 @@ function Portfolio() {
     return (
         <Container>
             <Col md={12}>
-                <Card className="mt-4">
-
-                    <div className="banner d-flex justify-content-center image-cap">
-                        <div className="image-cap-cut"> 
-                        
-                            {/* <!-- Portfolio about profile image --> */}
-                            <Card.Img className="goop rounded-circle shadow-lg p-1" src="/images/sebas_headshot.png" alt="Picture of Sebastian Arrazola" data-holder-rendered="true" />
-                    
-                        </div>
-                    </div>
-
-                    {/* <!-- Card Title --> */}
-                    <Card.Title className="d-flex justify-content-center mt-2">About Me</Card.Title>
-                    
-                    {/* <!-- line divider between heading and image --> */}
-                    <div className="line-short"/>
+                <CardDeck className="mt-4">
 
                     <Card.Body id="grad1">
 
-                        <div className="about-me-text">
-                            <p>Originally from Bogota, Colombia, my family and I emigrated to the states in search of peace away from the narco-state that had become our home at the time. The year was 1997, and we were off to the land of opportunity where we found a pleasant place to call home in Montgomery County, Maryland.</p>
-                            <p>Over the next 20 years I spent living in Montgomery County, I grew passionate for music production, engineering and mechanical design, as these fields offered a vessel for my creativity to flourish. Due to a growing interest in design and engineering I decided to study mechanical engineering at the University of Maryland. It was in this institution where I was exposed to mechatronics which is where mechanical, electrical, and software/computer engineering overlap.</p>
-                            <p>During my time in college, and after graduation I worked on various mechatronics, and product design projects such as active aero, design and CNC manufacturing of an ultra slim billet aluminum oil pan for the formula SAE race team I took part in. For my final mechatronics’ class project I designed and built a desk top Peltier HVAC system using a standalone atmega328p microcontroller that I programmed to interface with a user through a control knob and display for dialing-in a desired temperature set point. This project further spun off as an English class project in which I created educational material that aimed to walk students through the fundamentals of electronics  before having to follow a step-by-step manual on how to construct the Peltier HVAC system. The educational material/ build manual was distributed through a collaboration with the mpambara-cox foundation to high school students in Uganda that expressed interested in stem!</p>
-                            <p>While there were so many other projects, I inevitably fell in love with the software side of mechatronics. I really like how quickly, effortlessly software enables the deployment of great project ideas. While working in my second year at Ford Motor Company, I enrolled in an introductory course on microprocessors at the University of Michigan. Through this class I was exposed to the very hardware that makes software possible, and the programing language that makes it all possible behind the scenes, assembly. Working with assembly language opened my mind to a new-found understanding for how software manages hard-coded commands stored in memory, to make the magic of computation happen. From there it all clicked; I spent a decade “close to the metal” and now I am fully prepared to take on web and software development. Join me, as I attempt to speed-run to the top ranks of tech!</p>
-                            <p>Welcome to my first responsive site! I hope to connect, collaborate with you, and hear about your story.</p>
-                        </div>
-                    
+                        <Card.Title>Portfolio</Card.Title>
+
+                        {/* <!-- line divider between heading and image --> */}
+                        <div className="line" />
+
+                        {/* <!-- Card Container --------------------------> */}
+
+                        {/* <!-- Card Row 1 ------------------------------> */}
+                        <Row>
+                            <Col md={4}>
+                                <Card className="card shadow p-2 mt-4">
+                                    <Card.Body className="portfolio-card-body">
+                                        <Card.Title className="portfolio-card-title">
+                                            <h6>Avalon - Game Web App</h6>
+                                        </Card.Title>
+                                        <Row>
+                                            <a href="https://avalon-web-app.herokuapp.com/"><img className="img-fluid shadow" src="images/Avalon.gif" alt="WeatherDine" /></a>
+                                        </Row>
+                                        <Row className="mt-2 justify-content-center">
+                                            {/* <!-- Git Hub Profile Link --> */}
+                                            <a className="portlolio-link" href="https://github.com/AlecWGreene/MSUProject2Team4"><i className="fab fa-github fa-1x"></i></a>
+                                        </Row>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+
+                            <Col md={4}>
+                                <Card className="card shadow p-2 mt-4">
+                                    <Card.Body className="portfolio-card-body">
+                                        <Card.Title className="portfolio-card-title">
+                                            <h6>WeatherDine</h6>
+                                        </Card.Title>
+                                        <Row>
+                                            <a href="https://chidinmanze.github.io/project1/"><img className="img-fluid shadow" src="images/WeatherDine.gif" alt="WeatherDine" /></a>
+                                        </Row>
+                                        <Row className="mt-2 justify-content-center">
+                                            {/* <!-- Git Hub Profile Link --> */}
+                                            <a className="portlolio-link" href="https://github.com/chidinmanze/project1"><i className="fab fa-github fa-1x"></i></a>
+                                        </Row>
+                                    </Card.Body>
+                                </Card>
+                            </Col>  
+
+                            <Col md={4}>
+                                <Card className="card shadow p-2 mt-4">
+                                    <Card.Body className="portfolio-card-body">
+                                        <Card.Title className="portfolio-card-title">
+                                            <h6>Employee Tracker</h6>
+                                        </Card.Title>
+                                        <Row>
+                                            <a href="https://github.com/SeaBa55/employee-tracker"><img className="img-fluid shadow" src="images/employee-tracker.gif" alt="weather-dashboard" /></a>
+                                        </Row>
+                                        <Row className="mt-2 justify-content-center">
+                                            {/* <!-- Git Hub Profile Link --> */}
+                                            <a className="portlolio-link" href="https://github.com/SeaBa55/employee-tracker"><i className="fab fa-github fa-1x"></i></a>
+                                        </Row>
+                                    </Card.Body>
+                                </Card>
+                            </Col>  
+                        </Row>
+                        {/* <!-- end: Card Row 1 -------------------------> */}
+
+                        {/* <!-- Card Row 2 ------------------------------> */}
+                        <Row>
+                            <Col md={4}>
+                                <Card className="card shadow p-2 mt-4">
+                                    <Card.Body className="portfolio-card-body">
+                                        <Card.Title className="portfolio-card-title">
+                                            <h6>Weather Dashboard</h6>
+                                        </Card.Title>
+                                        <Row>
+                                            <a href="https://seaba55.github.io/weather-dashboard/"><img className="img-fluid shadow" src="images/weather-dashboard.gif" alt="weather-dashboard" /></a>
+                                        </Row>
+                                        <Row className="mt-2 justify-content-center">
+                                            {/* <!-- Git Hub Profile Link --> */}
+                                            <a className="portlolio-link" href="https://github.com/SeaBa55/weather-dashboard"><i className="fab fa-github fa-1x"></i></a>
+                                        </Row>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+
+                            <Col md={4}>
+                                <Card className="card shadow p-2 mt-4">
+                                    <Card.Body className="portfolio-card-body">
+                                        <Card.Title className="portfolio-card-title">
+                                            <h6>Code Quiz</h6>
+                                        </Card.Title>
+                                        <Row>
+                                            <a href="https://seaba55.github.io/code-quiz/"><img className="img-fluid shadow" src="images/Coding-Quiz.gif" alt="coding-quiz" /></a>
+                                        </Row>
+                                        <Row className="mt-2 justify-content-center">
+                                            {/* <!-- Gi/t Hub Profile Link --> */}
+                                            <a className="portlolio-link" href="https://github.com/SeaBa55/code-quiz"><i className="fab fa-github fa-1x"></i></a>
+                                        </Row>
+                                    </Card.Body>
+                                </Card>
+                            </Col>  
+
+                            <Col md={4}>
+                                <Card className="card shadow p-2 mt-4">
+                                    <Card.Body className="portfolio-card-body">
+                                        <Card.Title className="portfolio-card-title">
+                                            <h6>Burger Tracker - Web App</h6>
+                                        </Card.Title>
+                                        <Row>
+                                            <a href="https://young-ocean-90529.herokuapp.com/"><img className="img-fluid shadow" src="images/Burgers!.gif" alt="work-day-scheduler" /></a>
+                                        </Row>
+                                        <Row className="mt-2 justify-content-center">
+                                            {/* <!-- Git Hub Profile Link --> */}
+                                            <a className="portlolio-link" href="https://github.com/SeaBa55/burger"><i className="fab fa-github fa-1x"></i></a>
+                                        </Row>
+                                    </Card.Body>
+                                </Card>
+                            </Col>  
+                        </Row>
+                        {/* <!-- end: Card Row 2 --------------------------> */}
+
+                    {/* <!-- Card Container ---------------------------> */}
                     </Card.Body>
-                </Card>
+                </CardDeck>
             </Col>
         </Container>
     );
