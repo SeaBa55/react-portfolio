7fd21567a28a1a246abfa2b52e2d46e1a86b8054
sarrazol <sebastianarrazola@gmail.com>
2021-04-23 14:55:33 -0400

    testing browser router on ghpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import history from "./utils/history";
 import Portfolio from "./pages/portfolio/index";
 import About from "./pages/about/index";
@@ -9,28 +9,20 @@ import Footer from "./components/footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { fab, fas, far } from '@fortawesome/free-brands-svg-icons'
-// import { faFilePdf, faGitHub, faLinkedin, faEnvelope } from '@fortawesome/free-solid-svg-icons'
-
-// library.add(fab, faGitHub, faLinkedin);
-// library.add(fas, faFilePdf);
-// library.add(far, faEnvelope);
-
 function App() {
   return (
     <div className="App">
     <NavBar/>
     <div className="page-content">
-      <Router basename={process.env.PUBLIC_URL} history={history}>
+      <Router history={history}>
         <Switch>
           <Route path="/" exact>
             <Portfolio />
           </Route>
-          <Route path="/about">
+          <Route path="/about" exact>
             <About />
           </Route>
-          <Route path="/contact">
+          <Route path="/contact" exact>
             <Contact />
           </Route>
         </Switch>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +8,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Router basemname={`/${process.env.PUBLIC_URL}`}> */}
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
-    {/* </Router> */}
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
